61d2c11bdd17a2c49ec24784dba7cbc83b330f68
BlueFZ <muhammadsyafi@student.ub.ac.id>
2022-12-25 19:05:49 +0700

    fwafa

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -17,14 +17,16 @@ function Landing() {
           <img src={Foto} alt="" />
         </div>
 
-        <h1>Happy Birthday Danis</h1>
-        <div className="p-landing">
-				<p>
-          Selamat ulang tahun Danis, gak kerasa yaaa udah 20 tahun sekarang.
-          Semoga kamu sehat selalu dan dilimpahkan banyak rezeki, aamiin... Aku
-          awalnya bingung mau ngasih kartu ucapan tapi kepikiran buat bikin ini.
-          Hope you Enjoy
-        </p>
+        <div className="h1-landing">
+					<h1>Happy Birthday Danis</h1>
+					<div className="p-landing">
+					<p>
+						Selamat ulang tahun Danis, gak kerasa yaaa udah 20 tahun sekarang.
+						Semoga kamu sehat selalu dan dilimpahkan banyak rezeki, aamiin... Aku
+						awalnya bingung mau ngasih kartu ucapan tapi kepikiran buat bikin ini.
+						Hope you Enjoy
+					</p>
+					</div>
 				</div>
         <div className="but-container">
           <Link to="/milestone">
